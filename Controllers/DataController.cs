ecbaa9c7f76ecb8b9183e6a3b0863688e43297cb
Raghavendra Pai <raghavendra.pai@zapcg.com>
2024-05-21 17:26:51 +0530

    Initial

---
+++ Controllers/DataController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace AccessJson.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<EntityAttributeValue>> Get()
+        {
+            try
+            {
+                // Read data from JSON file
+                string jsonFilePath = "data.json";
+                string jsonData = System.IO.File.ReadAllText(jsonFilePath);
+
+                // Deserialize JSON to list of EntityAttributeValue objects
+                var entities = JsonSerializer.Deserialize<List<EntityAttributeValue>>(jsonData);
+
+                // Return the deserialized data
+                return Ok(entities);
+            }
+            catch (System.Exception ex)
+            {
+                // Return internal server error if an exception occurs
+                return StatusCode(500, new { error = "Internal Server Error" });
+            }
+        }
+    }
+}
---
+++ EntityAttributeValue.cs
@@ -0,0 +1,10 @@
+﻿namespace AccessJson
+{
+    public class EntityAttributeValue
+    {
+        public int Id { get; set; }
+        public string Entity { get; set; }
+        public string Attribute { get; set; }
+        public string Value { get; set; }
+    }
+}
